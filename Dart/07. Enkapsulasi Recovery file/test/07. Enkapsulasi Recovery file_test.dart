b6236689c0077d5ad51abcb9b48a19756a8a861f
Hendri <ndrianggar@gmail.com>
2021-05-05 11:28:40 +0700

    // update 05-05-2021

---
+++ Dart/07. Enkapsulasi Recovery file/test/07. Enkapsulasi Recovery file_test.dart
@@ -0,0 +1,8 @@
+import 'package:07. Enkapsulasi Recovery file/07. Enkapsulasi Recovery file.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
