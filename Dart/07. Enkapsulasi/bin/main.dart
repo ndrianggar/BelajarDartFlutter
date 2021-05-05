493cb9b19c30239584db1e16438f870aa2fee6fe
Hendri <ndrianggar@gmail.com>
2021-05-05 11:10:18 +0700

    Update File persegi_panjang.dart dan main

--- Dart/07. Enkapsulasi/bin/main.dart
+++ Dart/07. Enkapsulasi/bin/main.dart
@@ -7,18 +7,39 @@
 // kita punya data integer maka harapan kita data tersebut diinput dengan sesuai type integer
 // dengan demikian kita harus mengecek data tsb sesuai atau tidak
 
-// pada hal inilah kita buat program dengan teknik Encapsulation (Pembungkusan)
+// pada hal inilah kita buat program dengan teknik Encapsulation (Pembungkusan data)
 import 'dart:io';
-import 'package:07. Enkapsulasi/lib/persegi_panjang.dart';
+import '../lib/persegi_panjang.dart';
 
 void main(List<String> arguments) {
 PersegiPanjang kotak1, kotak2;
 
   kotak1 = new PersegiPanjang(); // artinya kita membuat objek baru di persegi panjang, dan ditunjuk oleh kotak1
-  kotak1.panjang = 2; // isi data
-  kotak1.lebar =5; // isi data
+  kotak1.setPanjang(-2); // tidak bisa diakses jika ditambahkan underscore _ karena harus membuat getter setter
+                      // di dart tidak perlu membuat getter setter, cukup dengan properties
+  kotak1.lebar =-5; // isi data
+
+  //catatan : jika ingin bisa diakses maka harus membuat prantara (getter / setter)
+ // penjelasan berikutnya ada di file persegi_panjang.dart
 
   kotak2 = PersegiPanjang(); // yang direkomendasikan tanpa new
-  kotak2.panjang = double.tryParse(stdin.readLineSync());
+  kotak2.setPanjang(double.tryParse(stdin.readLineSync()));
   kotak2.lebar = double.tryParse(stdin.readLineSync());
+
+  print(kotak2.luas);
+  print(kotak1.getPangjang());
+
+  print(kotak1.lebar); // hanya memanggil property seperti field(data)
+
 }
+
+
+/*
+    Kesimpulan :
+    1. yang dijadikan method itu ialah apa yang bisa dilakukan
+    kelas tersebut
+    2. apa yang dimiliki oleh kelas jadikan field data
+
+    3. contoh pembuatan nama method dengan nama hitungluas(){} ini tidak tepat
+    yang tepat adalah luas(){}
+ */
--- Dart/07. Enkapsulasi/lib/persegi_panjang.dart
+++ Dart/07. Enkapsulasi/lib/persegi_panjang.dart
@@ -1,9 +1,65 @@
 class PersegiPanjang {
-  double panjang;
-  double lebar;
+  double _panjang; // tanda underscore adalah menunjukan type data private
+  double _lebar;
 
+  // Penjelasan getter dan setter
+  /*
+  *  untuk mengubah nilai private nilai data dengan setter
+  *  buat fungsi void namaFungsi
+  *  kenapa void? karena void tidak mengembalikan nilai apapun hanya merubah
+  *  formatnya : void namaFungsi(tipeData namaTampungan){
+  *     kondisiIF(namaTampungan < 0){
+  *     namaTampungan *= -1;
+  * }
+  *
+  * _dataYangDiProtect = namaTampungan;
+  *
+  *
+  * */
 
-  double hitungLuas() {
-    return this.panjang * this.lebar;
+  // property setter
+  void set lebar(double value){
+    if(value < 0){
+      value *=-1;
+    }
+    _lebar = value;
   }
+
+  // property getter
+  double get lebar{ // tidak perlu dibuat kurung buka dan tutup
+  // ketika memanggil di main tidak
+    // perlu menggunakan underscore hanya memanggil propertynya
+    return _lebar;
+  }
+
+  // setter
+  void setPanjang(double value) {
+    if (value < 0) { // dicek terlebih dahulu sesuai kondisi
+      value *=-5; // dikalikan data supaya tidak < 0
+    }
+    _panjang = value;
+  }
+
+  // getter
+  double getPangjang(){
+    return _panjang;
+  }
+
+
+  double hitungLuas() { // tidak tepat dirubah dengan membuat property dibawah
+    return this._panjang * this.lebar;
+  }
+
+  double get luas => this._panjang * this.lebar;
 }
+
+
+/*
+    Kesimpulan :
+    1. yang dijadikan method itu ialah apa yang bisa dilakukan
+    kelas tersebut
+    2. apa yang dimiliki oleh kelas jadikan field data
+
+    3. contoh pembuatan nama method dengan nama hitungluas(){} ini tidak tepat
+    yang tepat adalah luas(){}
+ */
