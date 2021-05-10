4324f7023c3b2ea24a9854d5eeac2bc0e33903c6
Hendri <ndrianggar@gmail.com>
2021-05-10 10:44:01 +0700

    Perbaiki coment dan menambahkan file yang hilang(flying_monster & monster_ucoa)

---
+++ Dart/09. Abstract class & Interface/lib/monster_ucoa.dart
@@ -0,0 +1,14 @@
+import 'flying_monster.dart';
+import 'monster_ubur_ubur.dart';
+
+class MonsterUcoa extends MonsterUburUbur implements FlyingMonster, BasketPlayer {
+  @override
+  String fly() {
+    return "Terbang terbang melayang";
+  }
+
+// misalnya buat abstract kelas untuk implement dibanyak kelas
+}
+abstract class BasketPlayer{
+// tinggal menambahkan di kelas2
+}
